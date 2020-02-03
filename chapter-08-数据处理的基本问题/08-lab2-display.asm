5c829cb0ae8aea0ec433c688e17f7ab0c223047c
lambdafate <lambdafate@163.com>
2020-02-03 12:55:18 +0800

    :wink: display calculate result!

---
+++ chapter-08-数据处理的基本问题/08-lab2-display.asm
@@ -0,0 +1,89 @@
+assume cs:code
+
+data segment 
+    db '1975', '1976', '1977', '1978', '1979', '1980', '1981', '1982', '1983'
+    db '1984', '1985', '1986', '1987', '1988', '1989', '1990', '1991', '1992'
+    db '1993', '1994', '1995'
+    ; 21个年份
+
+    dd 16, 22, 382, 1356, 2390, 8000, 16000, 24486, 50065, 97479, 140417, 197514
+    dd 345980, 590827, 803530, 1183000, 1843000, 2759000, 3753000, 4649000, 5937000
+    ; 21年的总收入
+
+    dw 3, 7, 9, 13, 28, 38, 130, 220, 476, 778, 1001, 1442, 2258, 2793, 4037, 5635, 8226
+    dw 11542, 14430, 15257, 17800
+    ; 21年公司雇的人数
+data ends
+
+
+table segment   
+    db 21 dup("year summ ne ?? ")
+table ends 
+
+code segment
+start:
+    mov ax, data
+    mov es, ax
+
+    mov si, 0
+    mov di, 168
+
+    mov ax, table
+    mov ds, ax
+    mov bx, 0
+
+    mov cx, 21
+s0: 
+    mov dx, es:[si+0]
+    mov ds:[bx+0], dx
+    mov dx, es:[si+2]
+    mov ds:[bx+2], dx
+
+    mov dx, es:[si+84+0]
+    mov ds:[bx+5+0], dx
+    mov dx, es:[si+84+2]
+    mov ds:[bx+5+2], dx
+
+    mov dx, es:[di]
+    mov ds:[bx+10], dx
+
+    ; 计算
+    mov ax, ds:[bx+5+0]
+    mov dx, ds:[bx+5+2]
+
+    div word ptr ds:[bx+10]
+    mov ds:[bx+13], ax
+
+    add si, 4
+    add di, 2
+    add bx, 16
+
+    loop s0
+
+    ; 把数字转换为字符
+    mov bx, 0
+    mov cx, 21
+s1: 
+    mov si, 15
+    mov ax, ds:[bx+13]
+    mov byte ptr ds:[bx+4], 10
+    mov dx, cx
+    mov cx, 5
+s2:
+    div byte ptr ds:[bx+4]
+    add ah, 30H
+    mov ds:[bx+si], ah
+    sub si, 1
+    mov ah, 0
+    loop s2
+
+    mov cx, dx
+    add bx, 16
+    loop s1
+
+    mov ax, 4c00H
+    int 21H
+
+code ends
+
+end start
